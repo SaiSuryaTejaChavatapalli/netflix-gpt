dfbd8acba8e39b7479bc67ce9312fa569fbca1e3
Sai Surya Teja <saisuryateja3@gmail.com>
2023-10-13 16:07:37 +0530

    added error msg for search comp

--- src/components/Gpt/GptSearchMovieSuggestions.jsx
+++ src/components/Gpt/GptSearchMovieSuggestions.jsx
@@ -5,6 +5,15 @@ const GptSearchMovieSuggestions = () => {
   const userSearchMovies = useSelector(
     (store) => store.movies.userSearchMovies
   );
+  console.log("userSearchMovies", userSearchMovies);
+  if (userSearchMovies.length === 0)
+    return (
+      <div className="flex justify-center items-center">
+        <div className="text-red-800 bg-black w-1/3 p-24 text-center text-4xl font-bold rounded-lg">
+          Not Found
+        </div>
+      </div>
+    );
   return (
     userSearchMovies && (
       <div className="bg-black m-4 p-2">
